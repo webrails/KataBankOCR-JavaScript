3cc6ff3faaa1a70c5c15eefca2f4ec6d2506cda8
Marc Bungart <dev@webrails.de>
2012-02-11 14:27:26 +0100

    Char value map

--- KataBankOCR.js
+++ KataBankOCR.js
@@ -16,7 +16,15 @@ function KataBankOCR() {
     };
 
     this.charsValueMap = {
-        "     |  |":"0"
+        "     |  |   ":"1",
+        " _   |      ":"2",
+        " _  _| _|   ":"3",
+        "   |_|  |   ":"4",
+        " _ |_  _|   ":"5",
+        " _ |_ |_|   ":"6",
+        " _   |  |   ":"7",
+        " _ |_||_|   ":"8",
+        " _ |_| _|   ":"9"
     };
 
     this.extractLinesFrom = function (entry) {
