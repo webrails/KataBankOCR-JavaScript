17da272c410679a98edfdb7b4faa81e0de1e55a6
Marc Bungart <dev@webrails.de>
2012-02-13 23:22:47 +0100

    Passing tests

--- KataBankOCR.js
+++ KataBankOCR.js
@@ -9,15 +9,16 @@ function KataBankOCR() {
         linesPerEntry:4,
         charsPerDigitPerLine:3,
         digitsPerLine:9,
-        charsPerLine:(function () {
-            return this.charsPerDigitPerLine * this.digitsPerLine;
-        })(),
+        charsPerLine:27, //(function () {
+//            console.log('hui');
+//            return this.config.charsPerDigitPerLine * this.config.digitsPerLine;
+//        })(),
         lineBreak:"\n"
     };
 
     this.charsValueMap = {
         "     |  |   ":"1",
-        " _   |      ":"2",
+        " _  _||_    ":"2",
         " _  _| _|   ":"3",
         "   |_|  |   ":"4",
         " _ |_  _|   ":"5",
@@ -27,19 +28,29 @@ function KataBankOCR() {
         " _ |_| _|   ":"9"
     };
 
-    this.extractLinesFrom = function (entry) {
-        return entry.split('\n');
-    };
-
     // Takes an array of lines that represent the number to return
     this.processEntry = function (entry) {
-        var self;
+        console.log('processEntry entry length: ' + entry.length);
+        console.log('processEntry entry: ' + entry);
+        var self, config, charsValueMap;
         self = this;
-        return parseInt(_.reduce(_.range(0, this.config.charsPerLine, this.config.charsPerDigitPerLine), function (numberChars, index) {
-            numberChars += self.charsValueMap(_.reduce(entry, function (digitChars, line) {
-                return digitChars + line.substr(index, self.config.charsPerDigitPerLine);
-            }, ""));
-        }, ""), 10);
+        config = self.config;
+        charsValueMap = self.charsValueMap;
+//        console.log(config);
+//        console.log(charsValueMap);
+        var returnReduce = _.reduce(_.range(0, config.charsPerLine, config.charsPerDigitPerLine),
+            function (numberChars, index) {
+                console.log('processEntry numberChars: ' + numberChars + 'END');
+                console.log('processEntry index: ' + index);
+//                var
+                return numberChars += charsValueMap[_.reduce(entry, function (digitChars, line) {
+                    console.log('line: ' + line);
+                    console.log('digitChars: ' + digitChars);
+                    return digitChars + line.substr(index, config.charsPerDigitPerLine);
+                }, "")];
+            }, "");
+        console.log('returnReduce: ' + returnReduce);
+        return(parseInt(returnReduce, 10))
     };
 
     // Takes a file and returns an Array of numbers representing the entries of the file
@@ -48,15 +59,26 @@ function KataBankOCR() {
         self = this;
         numbers = [];
         entry = [];
-        _.each(file.split(this.lineBreak), function (line, index) {
+//        console.log('file: ' + file + 'END');
+//        var howManyLines = file.split(self.config.lineBreak);
+//        console.log('howManyLines: ' + howManyLines.length);
+        _.each(file.split(self.config.lineBreak), function (line, index) {
+
+//            console.log('line: ' + line);
+//            console.log('index: ' + index);
             entry.push(line);
             if (index === self.config.linesPerEntry - 1) {
+//                console.log('entry length: ' + entry.length);
+//                console.log('entry: ' + entry);
                 numbers.push(self.processEntry(entry));
                 entry = [];
             }
         });
+
+//        _.reduce(file.split(this.lineBreak), function() {
+//
+//        }, []);
+        console.log('numbers: ' + numbers);
         return numbers;
     };
 }
-
-
--- KataBankOCR.spec.js
+++ KataBankOCR.spec.js
@@ -1,18 +1,40 @@
 (function () {
     "use strict";
     describe("KataBankOCR", function () {
+        var kataBankOCR;
         beforeEach(function () {
             this.kataBankOCR = new KataBankOCR();
+            this.test123456789 = "    _  _     _  _  _  _  _ " + "\n" +
+                                 "  | _| _||_||_ |_   ||_||_|" + "\n" +
+                                 "  ||_  _|  | _||_|  ||_| _|" + "\n" +
+                                 "                           ";
+            this.test123456781 = "    _  _     _  _  _  _    " + "\n" +
+                                 "  | _| _||_||_ |_   ||_|  |" + "\n" +
+                                 "  ||_  _|  | _||_|  ||_|  |" + "\n" +
+                                 "                           ";
         });
         it("should be instantiable", function () {
             expect(this.kataBankOCR).toBeTruthy();
         });
-        describe("run", function () {
+        describe("Process file", function () {
             it("should be a function", function () {
-                expect(_.isFunction(this.kataBankOCR.processEntry)).toBeTruthy();
+                expect(_.isFunction(this.kataBankOCR.processFile)).toBeTruthy();
             });
-            it("should return an Integer", function () {
-                expect(_.isNumber(this.kataBankOCR.processEntry("hhh"))).toBeTruthy();
+            xit("should return an empty Array", function () {
+                expect(_.isArray(this.kataBankOCR.processFile(""))).toBeTruthy();
+                expect(this.kataBankOCR.processFile("")).toEqual([]);
+            });
+            it("should return an Array with one argument", function () {
+                expect(_.isArray(this.kataBankOCR.processFile(this.test123456789))).toBeTruthy();
+                expect(this.kataBankOCR.processFile(this.test123456789).length).toEqual(1);
+                expect(this.kataBankOCR.processFile(this.test123456789)[0]).toEqual(123456789);
+            });
+            xit("should return an Array with two arguments", function () {
+                var fileWithTwoEntries = this.test123456789 + "\n" + this.test123456781;
+                expect(_.isArray(this.kataBankOCR.processFile(fileWithTwoEntries))).toBeTruthy();
+                expect(this.kataBankOCR.processFile(fileWithTwoEntries).length).toEqual(2);
+                expect(this.kataBankOCR.processFile(fileWithTwoEntries)[0]).toEqual(123456789);
+                expect(this.kataBankOCR.processFile(fileWithTwoEntries)[1]).toEqual(123456781);
             });
         });
     });
